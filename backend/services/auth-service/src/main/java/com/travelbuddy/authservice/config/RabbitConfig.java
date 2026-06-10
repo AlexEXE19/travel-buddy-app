a4f4c06c39c38f59c103dd9d922a7ae555c5eeac
Alex <alexutzu873@gmail.com>
2026-06-10 15:09:07 +0300

    docker: updated docker files with proper version

--- backend/services/auth-service/src/main/java/com/travelbuddy/authservice/config/RabbitConfig.java
+++ backend/services/auth-service/src/main/java/com/travelbuddy/authservice/config/RabbitConfig.java
@@ -1,4 +1,34 @@
 package com.travelbuddy.authservice.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
 public class RabbitConfig {
+
+    public static final String QUEUE = "user.queue";
+    public static final String EXCHANGE = "user.exchange";
+    public static final String ROUTING_KEY = "user.created";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(ROUTING_KEY);
+    }
 }
--- backend/services/auth-service/src/main/java/com/travelbuddy/authservice/service/UserProducer.java
+++ backend/services/auth-service/src/main/java/com/travelbuddy/authservice/service/UserProducer.java
@@ -1,4 +1,24 @@
 package com.travelbuddy.authservice.service;
 
+import com.travelbuddy.authservice.config.RabbitConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+        public void sendUserCreatedEvent(String userId) {
+        rabbitTemplate.convertAndSend(
+                RabbitConfig.EXCHANGE,
+                RabbitConfig.ROUTING_KEY,
+                userId
+        );
+    }
 }
